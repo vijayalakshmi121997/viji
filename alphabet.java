f3714a2ca5fa9ba3237079ca44e00f5786c24565
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-27 13:05:48 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char c='*';
+		if(c == 'a' || c == 'b' )
+		System.out.println("Alphabet");
+		else
+		System.out.println("Not");
+	}
+}
