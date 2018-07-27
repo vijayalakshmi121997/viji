7f3cf9296cce6b3b756231ed964dccda336b1b6f
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-27 12:53:01 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,12 @@
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='i';
+		if(ch == 'a'|| ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+		System.out.println("Vowel");
+		else
+		System.out.println("Consonant");
+	}
+}
