7769b3fb1a363ce72b2652d8b65a3443a19d625f
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-07-27 12:42:30 +0530

    Create evens.java

---
+++ evens.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner reader=new Scanner(System.in);
+
+		System.out.print("enter a number:");
+		int num =reader.nextInt();
+		if (num % 2 == 0)
+		System.out.print("Even");
+		else 
+		System.out.print("Odd");
+	
+	}
+}
